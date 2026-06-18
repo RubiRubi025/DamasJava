351aeb3ead5c86656bfa9705556a9cd5c5892fad
RubiRubi025 <quebella54321@gmail.com>
2026-06-17 22:27:02 -0400

    Motor de red terminado: ConexionCliente y ConexionServidor

---
+++ ConexionCliente.java
@@ -0,0 +1,88 @@
+package damas;
+
+//Importaciones necesarias para la parte de red del cliente
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+//Clase que representa la conexión del cliente para el juego de damas, esto es un hilo 
+//que se ejecuta en el fondo para manejar la comunicación con el servidor sin congelar la pantalla del cliente
+public class ConexionCliente implements Runnable {
+
+    //Atributos para la conexión del cliente
+    private Socket cliente;
+    private String direccionIP;
+    private int puerto;
+
+    //Streams/embudos para enviar y recibir datos entre el cliente y el servidor
+    private DataInputStream entrada;
+    private DataOutputStream salida;
+
+    //Constructor que inicializa la dirección IP y el puerto del servidor al que el cliente se conectará
+    public ConexionCliente(String direccionIP, int puerto){
+        this.direccionIP = direccionIP; // Guarda la dirección IP del servidor
+        this.puerto = puerto; // Guarda el número de puerto del servidor
+
+    }
+
+    @Override
+    public void run(){
+        //Bloque try/catch para manejar posibles errores de red
+        try {
+
+            //Intenta conectarse al servidor usando la dirección IP y el puerto especificados
+            cliente = new Socket(direccionIP, puerto);
+            System.out.println("¡Conectado al servidor con éxito!");
+
+            //Inicializa los streams para enviar y recibir datos
+            entrada = new DataInputStream(cliente.getInputStream());
+            salida = new DataOutputStream(cliente.getOutputStream());
+
+                //Este bucle permanece activo mientras el cliente esté corriendo,
+                //esperando y procesando mensajes del servidor
+
+                while (true) {
+                    
+                    String mensaje = entrada.readUTF();
+                    System.out.println("Mensaje recibido del servidor: " + mensaje);
+
+                    // Aquí se pueden agregar condiciones para procesar diferentes tipos de mensajes
+                String[] partesMensaje = mensaje.split(","); // Divide el mensaje en partes usando la coma como separador
+                String accion = partesMensaje[0]; // La primera parte del mensaje indica la acción a realizar
+                
+                //procesamiento de un mensaje de movimiento
+                if (accion.equals("MOVER")){
+
+                    //Traducción de las partes del mensaje que son texto a números enteros.
+                    int filaOrigen = Integer.parseInt(partesMensaje[1]); 
+                    int columnaOrigen = Integer.parseInt(partesMensaje[2]);
+                    int filaDestino = Integer.parseInt(partesMensaje[3]);
+                    int columnaDestino = Integer.parseInt(partesMensaje[4]);
+
+                    //Se imprime en la consola del servidor el movimiento que el cliente ha realizado,
+                    //mostrando las coordenadas de origen y destino de la ficha movida.
+                    System.out.println("El rival movió la ficha de [" + filaOrigen + "," + columnaOrigen + "] a [" + filaDestino + "," + columnaDestino + "]");
+
+                }
+                }
+
+            } catch (Exception e) {
+            
+            //Si ocurre un error, se imprime el mensaje de error en la consola
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    //Método para enviar un mensaje al servidor
+    public void enviarMensaje(String mensaje){
+        try {
+            salida.writeUTF(mensaje);
+            
+        } catch (Exception e) {
+            System.out.println("Error al enviar: " + e.getMessage());
+
+        }
+    }
+
+}
+    
---
+++ ConexionServidor.java
@@ -0,0 +1,91 @@
+package damas;
+
+//Importaciones son necesarias para la parte de red del servidor
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//Clase que representa la conexión del servidor para el juego de damas
+// La palabra "implements Runnable" convierte esta clase en un Hilo (Thread) 
+// que puede correr en el fondo sin congelar la pantalla.
+public class ConexionServidor implements Runnable {
+
+    // Atributos para la conexión del servidor
+    private int puerto;
+    private ServerSocket servidor;
+    private Socket cliente;
+
+    // Streams/embudos para enviar y recibir datos entre el servidor y el cliente
+    private DataInputStream entrada;
+    private DataOutputStream salida;
+
+    // Constructor que inicializa el puerto del servidor
+    public ConexionServidor(int puerto) {
+
+        this.puerto = puerto; // Guarda el número de puerto para el servidor
+
+    }
+
+    // Método que se ejecuta cuando el hilo del servidor comienza a correr
+    @Override
+    public void run(){
+
+        // Bloque try/catch para manejar posibles errores de red
+        try {
+
+            servidor= new ServerSocket(puerto); // Crea un servidor que escucha en el puerto especificado
+            System.out.println("Servidor abierto. Esperando al jugador 2... ");
+            cliente = servidor.accept(); // Espera a que un cliente se conecte y acepta la conexión
+            System.out.println("¡Cliente conectado con éxito!");
+
+            // Inicializa los streams para enviar y recibir datos
+            entrada = new DataInputStream(cliente.getInputStream()); // Stream para recibir datos del cliente
+            salida = new DataOutputStream(cliente.getOutputStream()); // Stream para enviar datos al cliente
+
+            //Este bucle permanece activo mientras el servidor 
+            //esté corriendo, esperando y procesando mensajes del cliente
+            while(true){
+
+                String mensaje = entrada.readUTF();
+                System.out.println("Mensaje recibido del cliente: " + mensaje);
+
+                // Aquí se pueden agregar condiciones para procesar diferentes tipos de mensajes
+                String[] partesMensaje = mensaje.split(","); // Divide el mensaje en partes usando la coma como separador
+                String accion = partesMensaje[0]; // La primera parte del mensaje indica la acción a realizar
+                
+                //procesamiento de un mensaje de movimiento
+                if (accion.equals("MOVER")){
+
+                    //Traducción de las partes del mensaje que son texto a números enteros.
+                    int filaOrigen = Integer.parseInt(partesMensaje[1]); 
+                    int columnaOrigen = Integer.parseInt(partesMensaje[2]);
+                    int filaDestino = Integer.parseInt(partesMensaje[3]);
+                    int columnaDestino = Integer.parseInt(partesMensaje[4]);
+
+                    //Se imprime en la consola del servidor el movimiento que el cliente ha realizado,
+                    //mostrando las coordenadas de origen y destino de la ficha movida.
+                    System.out.println("El rival movió la ficha de [" + filaOrigen + "," + columnaOrigen + "] a [" + filaDestino + "," + columnaDestino + "]");
+
+                }
+
+            }
+
+        } catch (Exception e) {
+
+            //Si ocurre un error, se imprime el mensaje de error en la consola
+            System.out.println("Error: " + e.getMessage());
+
+        }
+    }
+    
+    // Método para enviar un mensaje al cliente
+    public void enviarMensaje(String mensaje){
+        try {
+            salida.writeUTF(mensaje);
+
+        } catch (Exception e) {
+            System.out.println("Error al enviar: " + e.getMessage());
+        }
+    }
+}
